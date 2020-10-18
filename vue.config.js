4a8fa3b64511a2b778094df12a3cf501453d14c3
Resita Sri Wahyuni <resita@alterra.id>
2020-10-18 20:06:57 +0800

    ayodonglah

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     runtimeCompiler: true,
-    publicPath: '/projek-himit/',
+    publicPath: '/',
     // publicPath:
     // process.env.NODE_ENV === "production" ? "/" : "/",
 
